05c7984c87f7e951b6044f34dea5c79e83c36feb
Benedikt Heuser <ben.heuser@stud.unibas.ch>
2023-03-23 16:52:50 +0100

    Benana

--- Uebung1/Aufgabe2/server.c
+++ Uebung1/Aufgabe2/server.c
@@ -1,131 +1,62 @@
-#include <netinet/in.h>
+// server program for udp connection
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <unistd.h>
-#include <pthread.h>
-#define PORT 6969
+#include<netinet/in.h>
+#define PORT 5000
+#define MAXLINE 1000
 
-int main(int argc, char const* argv[])
+struct name_ipport
 {
-    
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
+    char name[30];
+    char ipport[30];
+};
 
-    /* 
-    Creating server socket, 
-    server_fd is -1 if failed
-    AF_INET means it's IPv4
-    SOCK_STREAM means it's a tcp connection :)
-    */
-
-    int server_socket;
-
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-
-
-    /*
-    Now we have to bind the socket to an ip address
-    */
-
-    struct sockaddr_in my_addr; // comes from one of the libraries, don't know which one
-    int addrlen = sizeof(my_addr); // length of the address structure because we give the pointer as a param
-
-    bzero(&my_addr, sizeof(my_addr)); // i have no idea wth this does but I found it online hehe
-    my_addr.sin_family = AF_INET; // has to match the socket() call
-    my_addr.sin_port = htons(PORT); // specify port to listen on
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY); //allow the server to accept a client connection on any interface
-    
-    // " (struct sockaddr*)&my_addr " is just ugly because it's c, in java this would be " SocketAdress myAddress "
-    if (bind(server_socket, (struct sockaddr*)&my_addr, addrlen) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
   
-    /*
-    Now the server is ready to listen for new connections.
-    Only servers need to listen. 
-    This does NOT mean that it connects to a new connection, it just listens... 
-    the second argument in listen() is int backlog
-    = the maximum number of pending connections the kernel should queue for the socket
-    */
+// Driver code
+int main()
+{   
 
-    if (listen(server_socket, 10) < 0) { 
-        perror("listen failed");
-        exit(EXIT_FAILURE);
+    struct name_ipport data[10];
+
+
+
+    char buffer[100];
+    char *message = "Hello Client";
+    int listenfd, len;
+    struct sockaddr_in servaddr, cliaddr;
+    bzero(&servaddr, sizeof(servaddr));
+  
+    // Create a UDP Socket
+    listenfd = socket(AF_INET, SOCK_DGRAM, 0);        
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_family = AF_INET; 
+   
+    // bind server address to socket descriptor
+    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+       
+    //receive the datagram
+
+    while (1) {
+
+        // revieve message
+
+        //if message log in message then save data
+
+        //if message get info message the giva data
+
     }
 
-    /*
-    Now the server is ready to accept the new connections.
-    int accept (int sockfd, struct sockaddr *fromaddr, socklen_t *addrlen)
-    */
-
-    int client_socket, valread;
-
-    if ((client_socket = accept(server_socket, (struct sockaddr*)&my_addr, (socklen_t*)&addrlen)) < 0) {
-        perror("accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    /*
-    usage: read (sockfd, buffer, ssize_t N);
-    the read() function reads N bytes of input into the memory area indicated by buffer.
-    */ 
-
-    valread = read(client_socket, buffer, 1024);
-    printf("%s\n", buffer);
-
-    send(client_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    
-
-    /*
-    network protocol:
-
-    Log in:
-    l:127.0.0.1:4000:name
-
-    get info of another client:
-    i:name
-
-    response: 127.0.0.1:4000
-    */
-
-    while (strcmp(buffer, "stop") != 0) {
-
-        // clear buffer
-        for (int i = 0; i < 1024; i++) {
-            buffer[i] = '\0';
-        }
-
-        valread = read(client_socket, buffer, 1024);
-        // print for debug
-        printf("%s\n", buffer);
-
-        if (strcmp(buffer[0], 'l') == 0) {
-            
-
-        }
-        if (strcmp(buffer))
-        sleep(1);
-        
-    }
-
-    /*
-    We have to close the socket after we're done 'cause reasons.
-    */
-    close(client_socket);
-    
-
-    /*
-    Also close the server socket.
-    */
-    shutdown(server_socket, SHUT_RDWR);
-    return 0;
-    
+    len = sizeof(cliaddr);
+    int n = recvfrom(listenfd, buffer, sizeof(buffer),
+            0, (struct sockaddr*)&cliaddr,&len); //receive message from server
+    buffer[n] = '\0';
+    puts(buffer);
+           
+    // send the response
+    sendto(listenfd, message, MAXLINE, 0,
+          (struct sockaddr*)&cliaddr, sizeof(cliaddr));
 }
