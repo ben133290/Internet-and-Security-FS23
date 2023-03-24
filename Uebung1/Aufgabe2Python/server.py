b0303775c498b3a33a7ff2df7a5f8389272953d1
Benedikt Heuser <ben.heuser@stud.unibas.ch>
2023-03-24 12:30:43 +0100

    create udp server

---
+++ Uebung1/Aufgabe2Python/server.py
@@ -0,0 +1,43 @@
+import sys
+import socket
+import threading
+
+class Person:
+  def __init__(self, name, ip, port):
+    self.name = name
+    self.ip = ip
+    self.port = port
+       
+
+server_address = "127.0.0.1"
+server_port = 5000
+
+newest_message = ""
+
+list_of_people = []
+
+# create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+
+while True:
+
+    data, addr = server_socket.recvfrom(1024)
+    message = str(data, 'utf-8')
+    print("received message: %s" % newest_message)
+
+    words = newest_message.split()
+    if words[0] == "l":
+        new_person = Person(words[1], words[2], words[3])
+        list_of_people.append(new_person)
+    
+    elif words[0] == "n":
+       for person in list_of_people:
+          if person.name == words[1]:
+            response = "" + person.ip + " " + person.port 
+            server_socket.sendto(response, addr)
+    
+
+
